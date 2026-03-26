713250fe669a36aa3d30f153344944b799babed8
Abderhman Elmarakby <abdulrahman_690a@ai.kfs.edu.eg>
2026-03-26 23:19:01 +0200

    Add files via upload

--- A_Elmarakby_Shutdown.bat
+++ A_Elmarakby_Shutdown.bat
@@ -1,3 +1,10 @@
+<# :
+@echo off
+powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((Get-Content '%~f0') -join \"`n\")"
+exit /b
+#>
+
+# Load Windows Forms and Drawing libraries
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
@@ -27,7 +34,7 @@ $textBox.Location = New-Object System.Drawing.Point(20, 45)
 $textBox.Size = New-Object System.Drawing.Size(275, 25)
 $textBox.Text = "1"
 
-# Start Button
+# Start Button (Blue)
 $btnOK = New-Object System.Windows.Forms.Button
 $btnOK.Text = "Start Timer"
 $btnOK.Location = New-Object System.Drawing.Point(20, 85)
@@ -36,7 +43,7 @@ $btnOK.BackColor = [System.Drawing.Color]::FromArgb(0, 120, 215)
 $btnOK.ForeColor = [System.Drawing.Color]::White
 $btnOK.FlatStyle = 'Flat'
 
-# Cancel Button
+# Cancel Button (Red)
 $btnCancel = New-Object System.Windows.Forms.Button
 $btnCancel.Text = "Cancel Timer"
 $btnCancel.Location = New-Object System.Drawing.Point(165, 85)
@@ -45,7 +52,7 @@ $btnCancel.BackColor = [System.Drawing.Color]::FromArgb(220, 53, 69)
 $btnCancel.ForeColor = [System.Drawing.Color]::White
 $btnCancel.FlatStyle = 'Flat'
 
-# Info Button
+# Info / LinkedIn Button (LinkedIn Blue)
 $btnInfo = New-Object System.Windows.Forms.Button
 $btnInfo.Text = "Info (LinkedIn)"
 $btnInfo.Location = New-Object System.Drawing.Point(20, 130)
@@ -54,39 +61,47 @@ $btnInfo.BackColor = [System.Drawing.Color]::FromArgb(10, 102, 194)
 $btnInfo.ForeColor = [System.Drawing.Color]::White
 $btnInfo.FlatStyle = 'Flat'
 
-# Actions
+# Start Button Actions
 $btnOK.Add_Click({
     $hours = $textBox.Text
     try {
         $seconds = [math]::Round([double]$hours * 3600)
         & shutdown -s -t $seconds
+        # Play Custom Success Sound (Rising tones)
         [console]::beep(1000, 200)
         [console]::beep(1500, 200)
         [System.Windows.Forms.MessageBox]::Show("The computer will shut down in $hours hour(s).", "A-Elmarakby", 0, [System.Windows.Forms.MessageBoxIcon]::Information)
         $form.Close()
     } catch {
+        # Play Custom Error Sound (Low buzz)
         [console]::beep(400, 300)
         [console]::beep(400, 300)
         [System.Windows.Forms.MessageBox]::Show("Please enter numbers only.", "Error", 0, [System.Windows.Forms.MessageBoxIcon]::Error)
     }
 })
 
+# Cancel Button Actions
 $btnCancel.Add_Click({
     & shutdown -a
+    # Play Custom Cancel Sound (Falling tones)
     [console]::beep(1500, 200)
     [console]::beep(1000, 200)
     [System.Windows.Forms.MessageBox]::Show("Shutdown timer has been cancelled.", "A-Elmarakby", 0, [System.Windows.Forms.MessageBoxIcon]::Information)
     $form.Close()
 })
 
+# Info Button Actions
 $btnInfo.Add_Click({
+    # Play a Happy/Pleasing Sound (Musical chord arpeggio)
     [console]::beep(523, 150)
     [console]::beep(659, 150)
     [console]::beep(784, 150)
     [console]::beep(1046, 200)
+    # Open LinkedIn Profile
     Start-Process "https://www.linkedin.com/in/a-elmarakby"
 })
 
+# Add controls to the form and display it
 $form.Controls.Add($label)
 $form.Controls.Add($textBox)
 $form.Controls.Add($btnOK)
